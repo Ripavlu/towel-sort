4ca9ea6390186f563f99364652713dd504d1ba4b
RiPavlova <vlas0va-irina@mail.ru>
2024-10-25 00:05:19 +0700

    init: add answear

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == null) {
+    return [];
+  }
+  let result = [];
+  let k = true;
+  for (let row of matrix) {
+    if (k == true) {
+      for (i = 0; i < row.length; i++) {
+        let a = row[i];
+        result.push(a);
+      }
+      k = !k;
+    }
+    else {
+      for (i = row.length-1; i >= 0; i--) {
+        let a = row[i];
+        result.push(a);
+      }
+      k = !k;
+    }
+    
+
+  }
+  return result;
 }
